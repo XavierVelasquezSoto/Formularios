9f0b4c4ef0268a7399f3350f3815f60b758cc04f
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2024-11-05 21:40:25 +0100

    .

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+const formElement = document.getElementById("form")
+
+const formInfo = event => {
+    event.preventDefault(); //es importante para evitar que se actualicé la pagina
+}
+
+formElement.addEventListener("submit", formInfo) 
+
+
+/* estas lineas es necesario en todos los formularios, siempre
+
+const formElement = document.getElementById('form');
+const formInfo = event => {
+  event.preventDefault();
+};
+formElement.addEventListener('submit', formInfo); */
+
+
+/* console.log(formElement.age.value); */
+//formElement.elNomberQueLeHayasDado.value
+//formElement.addEventListener("input", printFormInfo)
