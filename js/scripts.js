d516c219d8ee18e11a3edff5c0e7cbbd8b066dcc
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2024-11-06 19:03:40 +0100

    change 06//11

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,90 @@
+const formElement = document.getElementById("form")
 
+const nameCardElement = document.getElementById("nameCard") //input
+const numberCardElement = document.getElementById("numberCard")
+const monthCardElement = document.getElementById("dateCard1")
+const yearCardElement = document.getElementById("dateCard2")
+const cvcCardElement = document.getElementById("cvcCard")
+
+const cardFrontNameElement = document.getElementById("cardColorName") //span
+const cardFrontNumberElement = document.getElementById("cardColorNumber")
+const cardMonthElement = document.getElementById("cardMonth")
+const cardYearElement = document.getElementById("cardYear")
+const cardCvcElement = document.getElementById("cardCvc")
+
+
+const cardFrontName = () => {
+  if (nameCardElement.value === "")
+    {
+    cardFrontNameElement.textContent = "JANE APPLESSED"
+    }
+  else{
+    cardFrontNameElement.textContent = nameCardElement.value.toUpperCase()
+  }
+
+}
+const cardFrontNumber = () => {
+  if (numberCardElement.value === "")
+    {
+      cardFrontNumberElement.textContent = "0000 0000 0000 0000"
+    }
+  else
+    {
+      cardFrontNumberElement.textContent = numberCardElement.value
+    }
+}
+const cardMonth = () => {
+  if (monthCardElement.value === "")
+    {
+      cardMonthElement.textContent = "00"
+    }
+    else
+    {
+      cardMonthElement.textContent = monthCardElement.value      
+    }
+}
+const cardYear = () => {
+  if (yearCardElement.value === "")
+    {
+      cardYearElement.textContent = "00"
+    }
+  else
+    {
+    cardYearElement.textContent = yearCardElement.value
+    }
+}
+const cardCVC = () => {
+  if (cvcCardElement.value === "")
+    {
+      cvcCardElement.textContent = "000"
+    }
+    else
+    {
+      cardCvcElement.textContent = cvcCardElement.value
+    }
+}
+
+
+
+const formInfo = event => {
+  event.preventDefault();
+
+  const name = formElement.name.value  
+  const number = formElement.number.value
+  const month = formElement.month.value
+  const year = formElement.year.value
+  const cvc = formElement.cvc.value
+  //console.log(form.age.value);
+  
+}
+
+nameCardElement.addEventListener("input", cardFrontName)
+numberCardElement.addEventListener("input", cardFrontNumber)
+monthCardElement.addEventListener("input", cardMonth)
+yearCardElement.addEventListener("input", cardYear)
+cvcCardElement.addEventListener("input", cardCVC)
+
+formElement.addEventListener("submit", formInfo)
 
 
 
